23a0c9488427fa13ed9b2f45b5a593b48f228948
Egor Mironov <doramifasolyasi@gmail.com>
2021-07-22 20:22:43 +0400

    my fits commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const block1 = document.querySelector(".one");
+const block2 = document.querySelector(".two");
+const block3 = document.querySelector(".three");
+
+
+block1.addEventListener("click", function(){
+    console.log(1)
+})
+block2.addEventListener("click", function(){
+    console.log(2)
+})
+block3.addEventListener("click", function(){
+    console.log(3)
+})
